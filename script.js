e69fc8d82e893416052a13d57a356bc7e050fb3a
Smith Thongbai <thonsm06@gmail.com>
2023-05-27 06:14:32 -0500

    Added css rules to containers

--- script.js
+++ script.js
@@ -1,34 +1,71 @@
 const body = document.querySelector('body');
-const container = document.createElement('div');
+const buttonContainer = document.createElement('div');
 const rockBtn = document.createElement('button');
 const paperBtn = document.createElement('button');
 const scissorsBtn = document.createElement('button');
+const selectionContainer = document.createElement('div');
+const pSelection = document.createElement('p');
+const cSelection = document.createElement('p');
+const scoreContainer = document.createElement('div');
+const roundResult = document.createElement('p');
+const pScore = document.createElement('p');
+const cScore = document.createElement('p');
 
-container.style.cssText = "display: flex; gap: 24px;"
+let playerScore = 0;
+let computerScore = 0;
+
+body.style.cssText = "display: flex; flex-direction: column; align-items: center; gap: 24px; padding: 24px;";
+buttonContainer.style.cssText = "display: flex; gap: 24px;"
+selectionContainer.style.cssText = "display: flex; gap: 32px; justify-content: space-evenly; align-items: center; border: 3px solid black; width: 600px; height: 150px; font-size: 32px;"
+scoreContainer.style.cssText = "display: flex; flex-direction: column; align-items: center;"
 
 rockBtn.textContent = "Rock";
 paperBtn.textContent = "Paper";
 scissorsBtn.textContent = "Scissors";
+roundResult.textContent = "Current Round Winner: ";
+pScore.textContent = "Your score: ";
+cScore.textContent = "Opponent score: ";
+
 
 rockBtn.classList.toggle('rock');
 paperBtn.classList.toggle('paper');
 scissorsBtn.classList.toggle('scissors');
 
-container.appendChild(rockBtn);
-container.appendChild(paperBtn);
-container.appendChild(scissorsBtn);
-body.appendChild(container);
+buttonContainer.appendChild(rockBtn);
+buttonContainer.appendChild(paperBtn);
+buttonContainer.appendChild(scissorsBtn);
+selectionContainer.appendChild(pSelection);
+selectionContainer.appendChild(cSelection);
+scoreContainer.appendChild(roundResult);
+scoreContainer.appendChild(pScore);
+scoreContainer.appendChild(cScore);
+body.appendChild(buttonContainer);
+body.appendChild(selectionContainer);
+body.appendChild(scoreContainer);
 
 const buttons = document.querySelectorAll('button');
 buttons.forEach(button => button.addEventListener('click', (e) => {
-    const computerSelection = getComputerChoice();
-    const playerSelection = e.target.textContent.toLowerCase();
     
+    const playerSelection = e.target.textContent.toLowerCase();
+    const computerSelection = getComputerChoice();
+    
+    pSelection.textContent = playerSelection.toUpperCase();
+    cSelection.textContent = computerSelection.toUpperCase();
+
     const result = playRound(playerSelection, computerSelection);
-    console.log(result);
+    checkResult(result); //update the score
+    roundResult.textContent = "Current Round Winner: ";//reset round result content
+    roundResult.textContent += result;
 }))
 
-
+function checkResult(result) {
+    if (result === "You Win!") {
+        playerScore++;
+    }
+    else if (result === "You Lose") {
+        computerScore++;
+    }
+}
 const choices = ["rock", "paper", "scissors"];
 //let playerSelection;
 //let computerSelection;
